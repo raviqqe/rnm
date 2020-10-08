d60914a9e8a045e76e3bc74f1043c75cc1e7f81a
Yota Toyama <raviqqe@gmail.com>
2020-10-08 01:02:35 +0000

    Generalize patttern

--- pattern.go
+++ pattern.go
@@ -1,8 +1,93 @@
 package main
 
-import "regexp"
+import (
+	"regexp"
 
-type compiledPattern struct {
+	"github.com/iancoleman/strcase"
+)
+
+type patternOptions struct {
+	convert func(string) string
+	head    delimiter
+	tail    delimiter
+}
+
+type pattern struct {
 	From *regexp.Regexp
 	To   string
+}
+
+var patternOptionsList = []*patternOptions{
+	{
+		func(s string) string {
+			return strcase.ToDelimited(s, ' ')
+		},
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		func(s string) string {
+			return strcase.ToScreamingDelimited(s, ' ', 0, true)
+		},
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		strcase.ToKebab,
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		strcase.ToScreamingKebab,
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		strcase.ToSnake,
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		strcase.ToScreamingSnake,
+		nonAlphabet,
+		nonAlphabet,
+	},
+	{
+		strcase.ToCamel,
+		lowerCase,
+		upperCase,
+	},
+	{
+		strcase.ToLowerCamel,
+		upperCase,
+		upperCase,
+	},
+}
+
+func compilePatterns(from string, to string) ([]*pattern, error) {
+	ps := make([]*pattern, 0, len(patternOptionsList))
+
+	for _, o := range patternOptionsList {
+		p, err := compilePattern(from, to, o)
+		if err != nil {
+			return nil, err
+		}
+
+		ps = append(ps, p)
+	}
+
+	return ps, nil
+}
+
+func compilePattern(from string, to string, o *patternOptions) (*pattern, error) {
+	r, err := regexp.Compile(
+		compileDelimiter(o.head, true) +
+			o.convert(from) +
+			compileDelimiter(o.tail, false),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pattern{r, "${1}" + o.convert(to) + "${2}"}, nil
 }
--- renamer.go
+++ renamer.go
@@ -1,38 +1,13 @@
 package main
 
-import (
-	"regexp"
-
-	"github.com/iancoleman/strcase"
-)
-
 type renamer struct {
-	patterns []compiledPattern
+	patterns []*pattern
 }
 
 func newRenamer(from string, to string) (*renamer, error) {
-	ps := []compiledPattern{}
-
-	for _, f := range [](func(string) string){
-		func(s string) string {
-			return strcase.ToDelimited(s, ' ')
-		},
-		func(s string) string {
-			return strcase.ToScreamingDelimited(s, ' ', 0, true)
-		},
-		strcase.ToCamel,
-		strcase.ToKebab,
-		strcase.ToLowerCamel,
-		strcase.ToScreamingKebab,
-		strcase.ToScreamingSnake,
-		strcase.ToSnake,
-	} {
-		r, err := regexp.Compile(f(from))
-		if err != nil {
-			return nil, err
-		}
-
-		ps = append(ps, compiledPattern{r, f(to)})
+	ps, err := compilePatterns(from, to)
+	if err != nil {
+		return nil, err
 	}
 
 	return &renamer{ps}, nil
@@ -40,7 +15,7 @@ func newRenamer(from string, to string) (*renamer, error) {
 
 func (r *renamer) Rename(s string) string {
 	for _, p := range r.patterns {
-		s = p.From.ReplaceAllLiteralString(s, p.To)
+		s = p.From.ReplaceAllString(s, p.To)
 	}
 
 	return s
