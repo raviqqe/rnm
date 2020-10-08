d0b45fc3292c4911e8acdc2da5ca6bbb8ad36d0b
Yota Toyama <raviqqe@gmail.com>
2020-10-08 02:55:57 +0000

    Add case config file

--- pattern.go
+++ pattern.go
@@ -2,75 +2,11 @@ package main
 
 import (
 	"regexp"
-
-	"github.com/iancoleman/strcase"
 )
-
-type caseConfig struct {
-	name    caseName
-	convert func(string) string
-	head    delimiter
-	tail    delimiter
-}
 
 type pattern struct {
 	From *regexp.Regexp
 	To   string
-}
-
-var caseConfigs = []*caseConfig{
-	{
-		camel,
-		strcase.ToLowerCamel,
-		nonAlphabet,
-		upperCase,
-	},
-	{
-		upperCamel,
-		strcase.ToCamel,
-		lowerCase,
-		upperCase,
-	},
-	{
-		kebab,
-		strcase.ToKebab,
-		nonAlphabet,
-		nonAlphabet,
-	},
-	{
-		upperKebab,
-		strcase.ToScreamingKebab,
-		nonAlphabet,
-		nonAlphabet,
-	},
-	{
-		snake,
-		strcase.ToSnake,
-		nonAlphabet,
-		nonAlphabet,
-	},
-	{
-		upperSnake,
-		strcase.ToScreamingSnake,
-		nonAlphabet,
-		nonAlphabet,
-	},
-	{
-		space,
-		func(s string) string {
-			return strcase.ToDelimited(s, ' ')
-		},
-		nonAlphabet,
-		nonAlphabet,
-	},
-	{
-		upperSpace,
-		func(s string) string {
-			return strcase.ToScreamingDelimited(s, ' ', 0, true)
-		},
-		nonAlphabet,
-		nonAlphabet,
-	},
 }
 
 func compilePatterns(from string, to string, enabled map[caseName]struct{}) ([]*pattern, error) {
