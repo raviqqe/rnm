31609962c8f839c00c0d8ee9518912fadc816cf2
Yota Toyama <raviqqe@gmail.com>
2020-10-10 23:24:31 +0000

    Test path globber with git

--- path_globber_test.go
+++ path_globber_test.go
@@ -32,3 +32,15 @@ func TestPathGlobberGlobRecursively(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, []string{"foo", "foo/foo"}, ss)
 }
+
+func TestPathGlobberDoNotIncludePathsNotIncludedInRepository(t *testing.T) {
+	fs := memfs.New()
+	err := util.WriteFile(fs, "foo", []byte("foo"), 0o222)
+	assert.Nil(t, err)
+
+	commitFiles(t, fs, []string{"bar"})
+
+	ss, err := newTestPathGlobber(fs).Glob(".")
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"bar"}, ss)
+}
