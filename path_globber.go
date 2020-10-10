f8f4f859e8b958512d4559c2e3e53f97a3008766
Yota Toyama <raviqqe@gmail.com>
2020-10-10 23:11:37 +0000

    Set map capacity

--- path_globber.go
+++ path_globber.go
@@ -48,7 +48,7 @@ func (g *pathGlobber) Glob(d string) ([]string, error) {
 }
 
 func intersectStringSets(ss, sss []string) []string {
-	sm := map[string]struct{}{}
+	sm := make(map[string]struct{}, len(sss))
 
 	for _, s := range ss {
 		sm[s] = struct{}{}
