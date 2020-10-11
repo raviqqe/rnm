7dc402c44c25e297e4b5e5d2a6dc5f9854d7dedd
Yota Toyama <raviqqe@gmail.com>
2020-10-10 18:16:11 -0700

    Fix usage (#23)
    * Fix usage
    
    * Bump version
    
    * Update usage

--- configuration.go
+++ configuration.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	version                     = "0.8.0"
+	version                     = "0.8.1"
 	errorChannelCapacity        = 512
 	maxOpenFiles                = 512
 	fileTypeDetectionBufferSize = 512
