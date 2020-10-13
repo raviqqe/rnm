8fbdc9d6bba3df199c85a9af8ac9311d31034407
Yota Toyama <raviqqe@gmail.com>
2020-10-12 21:47:43 -0700

    Bump version (#32)

--- configuration.go
+++ configuration.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	version                     = "0.9.0"
+	version                     = "0.9.1"
 	errorChannelCapacity        = 512
 	maxOpenFiles                = 512
 	fileTypeDetectionBufferSize = 512
