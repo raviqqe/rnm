6d706b765870f144d15f301d078fac0d300c0282
Yota Toyama <raviqqe@gmail.com>
2020-10-14 02:44:10 +0000

    Add comment

--- configuration.go
+++ configuration.go
@@ -3,6 +3,6 @@ package main
 const (
 	version                     = "0.9.2"
 	errorChannelCapacity        = 512
-	maxOpenFiles                = 200
+	maxOpenFiles                = 200 // lower than 256 on macOS by default
 	fileTypeDetectionBufferSize = 512
 )
