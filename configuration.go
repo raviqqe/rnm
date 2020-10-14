0d3728b52d0e901d85f7d90617eebc0c6d884fe8
Yota Toyama <raviqqe@gmail.com>
2020-10-14 02:43:04 +0000

    Lower max open files

--- configuration.go
+++ configuration.go
@@ -3,6 +3,6 @@ package main
 const (
 	version                     = "0.9.2"
 	errorChannelCapacity        = 512
-	maxOpenFiles                = 512
+	maxOpenFiles                = 200
 	fileTypeDetectionBufferSize = 512
 )
