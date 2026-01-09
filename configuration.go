5cead081e15bce4dce6453753b407a07304d5487
Yota Toyama <raviqqe@gmail.com>
2026-01-09 09:12:56 -0800

    Bump version (#174)

--- configuration.go
+++ configuration.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	version                     = "0.15.2"
+	version                     = "0.15.3"
 	errorChannelCapacity        = 512
 	maxOpenFiles                = 200 // lower than 256 on macOS by default
 	fileTypeDetectionBufferSize = 512
